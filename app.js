c32e5663943b1a83f77185eb78c1481461d6d8e2
Zeeshan Ashir <ashirbutt246@gmail.com>
2023-04-08 15:14:43 +0500

    MarkUP and Functionality almost done.

---
+++ app.js
@@ -0,0 +1,48 @@
+let subBtn = document.querySelector('#sub');
+let taskInp = document.querySelector('#text-input')
+let taskList = document.querySelector('#lists')
+let resetbtn = document.querySelector('#removeFinished');
+let totalTask = 0;
+
+
+const finishItem = (e) => {
+    let li = e.target.parentElement
+    let checkButton = e.target;
+    li.classList.add('finish');
+    checkButton.disabled = true;
+}
+
+resetbtn.addEventListener('click',()=>{
+    let lis = document.querySelectorAll('li');
+    for (const li of lis) {
+        if(li.classList.contains('finish'))
+            li.remove();    
+    }
+})
+
+const getNewItem = (value) => {
+    let li = document.createElement('li');
+    let label = document.createElement('label')
+    let checkButton = document.createElement('input')
+    checkButton.type = 'checkbox'
+    checkButton.id = totalTask;
+    checkButton.addEventListener('change', finishItem)
+    console.log(value);
+    label.innerText = value;
+    label.htmlFor = totalTask;
+    li.append(label, checkButton);
+    return li;
+}
+
+subBtn.addEventListener('click', () => {
+    let value = taskInp.value;
+    if (value == "") {
+        return;
+    }
+    totalTask++;
+    console.log(value);
+    taskList.append(getNewItem(value));
+    taskInp.value = "";
+})
+
+
